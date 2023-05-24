1a2be17ab2ea854cffd064034b4cd365e22c18c9
Brayan <regimenbrian@gmail.com>
2023-05-24 12:00:32 -0500

    archivos subidos por primera vez

---
+++ main.js
@@ -0,0 +1,105 @@
+const $ = (s) => document.querySelector(s);
+
+const $ta = $("#contenedor");
+const $a = $(".encriptar");
+const $h = $(".desencriptar");
+const $e = $(".resultado");
+const $w = $(".anuncio");
+let buttonClicked = false;
+
+let value;
+
+const llaves = {
+    e: "enter",
+    i: "imes",
+    a: "ai",
+    o: "ober",
+    u: "ufat"
+};
+
+// function validarTexto($ta) {
+//     const patron = /^[a-z]+$/; //define el patrón regular de datos aceptados
+//     return !patron.test($ta);
+// }
+function convertir() {
+    $ta.value = $ta.value.toLowerCase();
+}
+
+function encriptar(t) {
+    return t.replace(/[eiaou]/g, (k) => llaves[k]);
+}
+
+function desencriptar(t) {
+    return t.replace(/(enter|imes|ai|ober|ufat)/g, (k) => {
+        for (let c in llaves) {
+            if (llaves[c] === k) return c;
+        }
+    });
+}
+
+const imagen = document.querySelector(".mensaje");
+
+$ta.addEventListener("input", (a) => {
+    value = $ta.value;
+    if (value === '') {
+        imagen.style.display = 'block';
+    } else {
+        imagen.style.display = 'none';
+    }
+});
+
+$ta.addEventListener("blur", () => {
+    if (!buttonClicked) {
+        const value = $ta.value;
+        const patron = /^[a-z]+$/;
+        if (!patron.test(value)) {
+            Swal.fire({
+                title: 'Escribe solo letras minusculas y sin acentos',
+                icon: 'warning',
+                showConfirmButton: false
+            });
+        }
+    }
+    buttonClicked = false;
+});
+
+$a.addEventListener("click", () => {
+    const value = $ta.value;
+    $e.innerText = encriptar(value);
+    if (!value) return;
+    if (validarTexto(value)) return
+
+    $ta.focus();
+    Swal.fire({
+        title: 'Escribe solo letra minusculas y sin acentos',
+        icon: 'warning',
+        showConfirmButton: false
+    })
+});
+
+$h.addEventListener("click", () => {
+    $e.innerText = desencriptar(value);
+});
+
+//función que permite copiar
+document.querySelector(".copiar").addEventListener('click', () => {
+    let copyText = $e.innerHTML;
+    navigator.clipboard.writeText(copyText).then(() => {
+        Swal.fire({
+            title: "Mensaje copiado!!",
+            icon: "success"
+        });
+    });
+    $e.innerHTML = "";
+    $ta.value = "";
+    imagen.style.display = 'block';
+});
+
+//Este código permite escribir en tiempo real sobre el textarea
+var contenedor = document.getElementById('contenedor');
+contenedor.addEventListener("input", textModified);
+
+function textModified() {
+    var resultado = document.getElementsByClassName('resultado')[0];
+    resultado.innerHTML = contenedor.value;
+}
